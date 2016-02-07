4fc1f03ca0ea3c582ea0db7cda2b5a3b307e5597
jbitoniau <jmenuet@gmail.com>
2016-02-07 15:54:08 +0100

    Worked on converting a list of flash datetimes into a list of flash counts per slices of time

--- Electromon.py
+++ Electromon.py
@@ -1,8 +1,70 @@
 import sys
 import datetime
+import time
 import gspread
 import json
+import random
 from oauth2client.client import SignedJwtAssertionCredentials
+from threading import Thread
+
+class FlashDetector(Thread):
+
+	def __init__(self, flashTimes):
+		Thread.__init__(self)
+		self.flashTimes = flashTimes
+		
+	def run(self):
+		for val in range(100):
+			minMs = 500
+			maxMs = 4000
+			v = random.randint(minMs, maxMs) / 1000
+			time.sleep(v)
+			nowTime = datetime.datetime.now()
+			print ">>>>" + nowTime.strftime('%d/%m/%Y %H:%M:%S')
+			self.flashTimes.append( nowTime )
+
+class PerTimeSliceFlashCounter():
+
+	def __init__(self, timeSliceDurationInS ):
+		self.timeSliceDurationInS = timeSliceDurationInS
+
+	def processDateTimes( self, dateTimes ):
+		result = []
+		dateTimeIndex = 0
+		numDateTimes = len(dateTimes)
+		while ( dateTimeIndex<numDateTimes ):
+			numFlashesInSlice = 0
+			sliceStartDate =  dateTimes[dateTimeIndex].date()
+			sliceIndex = self._getTimeSliceIndex( dateTimes[dateTimeIndex] )
+			while ( dateTimeIndex<numDateTimes and self._getTimeSliceIndex(dateTimes[dateTimeIndex])==sliceIndex ):
+				numFlashesInSlice += 1
+				dateTimeIndex += 1
+
+			sliceStartTime = self._getTimeSliceStartTime(sliceIndex)
+			sliceStartDateTime = datetime.datetime( sliceStartDate.year, sliceStartDate.month, sliceStartDate.day, sliceStartTime[0], sliceStartTime[1], sliceStartTime[2] )
+			#print "#" + str(sliceIndex) + " " + str(sliceStartDateTime) +  " " + str(numFlashesInSlice)
+			result.append( (sliceStartDateTime, numFlashesInSlice) )
+		return result
+
+	def _getTimeSliceIndex( self, dateTime ):
+		numSeconds = (dateTime.hour * 3600) + (dateTime.minute * 60) + (dateTime.second) + (dateTime.microsecond/1000000)
+		timeSliceNumber = int(numSeconds / self.timeSliceDurationInS)
+		return timeSliceNumber
+
+	def _getTimeSliceStartTime( self, timeSliceIndex ):
+		numSeconds = timeSliceIndex * self.timeSliceDurationInS
+		hours = numSeconds / 3600
+		numSeconds -= hours*3600
+		minutes = numSeconds / 60
+		numSeconds -= minutes * 60 
+		seconds = numSeconds
+		return (hours, minutes, seconds)
+
+		
+class FlashCountSender(): 
+
+	def __init__(self ):
+		print 'hello'
 
 def connectAndDoStuff():
 
@@ -47,8 +109,31 @@ def connectAndDoStuff():
 	#	a.append( c )
 	#wks.append_row( a )
 
-connectAndDoStuff()
+#connectAndDoStuff()
 
-dd = datetime.datetime.now()
-#https://docs.python.org/2/library/datetime.html#strftime-strptime-behavior
-print(dd.strftime('%d/%m/%Y %H:%M:%S'))
+
+#print "hello"
+#obj = MyClass(12)
+#obj.showStuff()
+#obj.run()
+#obj.aStaticFunc()
+
+flashTimes = []
+flashTimes.append( datetime.datetime(2016, 02, 07, 12, 00, 00) )
+flashTimes.append( datetime.datetime(2016, 02, 07, 12, 00, 02) )
+flashTimes.append( datetime.datetime(2016, 02, 07, 12, 00, 03) )
+flashTimes.append( datetime.datetime(2016, 02, 07, 12, 00, 03, 500000 ) )
+flashTimes.append( datetime.datetime(2016, 02, 07, 12, 00, 9, 999000) )
+flashTimes.append( datetime.datetime(2016, 02, 07, 12, 00, 11) )
+flashTimes.append( datetime.datetime(2016, 02, 07, 12, 00, 12) )
+#thread1 = FlashDetector(flashTimes)
+#thread1.start()
+#time.sleep(8)
+print 'enum'
+for i, t in enumerate(flashTimes):
+	print t.strftime('%d/%m/%Y %H:%M:%S')
+		
+counter = PerTimeSliceFlashCounter(5)
+r = counter.processDateTimes( flashTimes )
+print '=>' + str(r)
+#thread1.join()
