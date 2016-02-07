57eed9869d107c897d21c28775ff3906ae9601c1
jbitoniau <jmenuet@gmail.com>
2016-02-07 12:24:16 +0100

    Playing with different ways of updating cells

--- Electromon.py
+++ Electromon.py
@@ -13,20 +13,39 @@ def connectAndDoStuff():
 	gc = gspread.authorize(credentials)
 
 	wks = gc.open("Electromon").sheet1
-	wks.update_acell('B2', "it's down there somewhere, let me take another look.")
+	#wks.update_acell('B2', "it's down there somewhere, let me take another look.")
 
 
-	val = int( wks.acell('B1').value )
-	print val
-	val = val + 1
-	wks.update_acell('B1', str( val ))
+	#val = int( wks.acell('B1').value )
+	#print val
+	#val = val + 1
+	#wks.update_acell('B1', str( val ))
 
-	c = wks.row_count
-	print "row count " + str(c)
+	#c = wks.row_count
+	#print "row count " + str(c)
 
 	#wks.add_rows(2)
-	for val in range(5):
-		wks.append_row( ['blabla', datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S'), val ] )
+	#for val in range(5):
+	#	wks.append_row( ['blabla', datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S'), val ] )
+	
+	# 1:28
+	#for i in range(100):
+	#	wks.update_cell( i+2, 1, datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S') )
+
+	# 0:40
+	a = []
+	for i in range(1, 100):
+		c = wks.cell( i, 1 )
+		c.value = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+		a.append( c )
+	wks.update_cells( a )
+
+	# 0:40
+	#a = []
+	#for i in range(1, 100):
+	#	c = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+	#	a.append( c )
+	#wks.append_row( a )
 
 connectAndDoStuff()
 
