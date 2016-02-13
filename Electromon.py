cf2869a78a19d6027f55871b9af2e346f3ad71c1
jbitoniau <jmenuet@gmail.com>
2016-02-13 19:19:45 +0100

    Reworked the code counting flashes per time slice

--- Electromon.py
+++ Electromon.py
@@ -103,15 +103,31 @@ class FlashDetector(threading.Thread):
 		self.lastTimeValue = 0
 
 	def run(self):
+
+		# If the difference between the current value and the previous one  
+		# is less than 2 percent of the previous value in absolute value,
+		# the new value is ignored because it's too close to the previous one
+		# We want a significant change
+		deltaRatioThreshold = 0.02		
+		lastValue = self.gpioTimeReader.readTime()
+		lastDelta = 1;
 		while ( not self.stopRequest ):
-			v = self.gpioTimeReader.readTime()
-			print 'value: ' + str(v)
-			if v<self.lastTimeValue: 
-				nowTime = datetime.datetime.now()
-				print "flash! #" + str(len(self.flashTimes)) + " " + nowTime.strftime('%d/%m/%Y %H:%M:%S')  
-				with self.lock:
-					self.flashTimes.append( nowTime )
-			self.lastTimeValue = v
+			value = self.gpioTimeReader.readTime()
+			delta = value - lastValue;
+			deltaRatio = delta / lastValue 	
+			if ( math.fabs(deltaRatio)>=deltaRatioThreshold ):
+				if ( lastDelta>0 and delta<0 ):
+					# A flash is detected when the signal was increasing and is now decreasing
+					nowTime = datetime.datetime.now()
+					#print "flash! #" + str(len(self.flashTimes)) + " " + nowTime.strftime('%d/%m/%Y %H:%M:%S') 
+					print "Dtor:> " + nowTime.strftime('%d/%m/%Y %H:%M:%S')
+					with self.lock:
+						self.flashTimes.append( nowTime )
+					#os.system( "aplay beep.wav" )
+				lastValue = value
+				lastDelta = delta
+			else:
+				pass 	# We skip this value as it's too close to the previous
 
 	def flushFlashTimes(self):
 		with self.lock:
@@ -125,53 +141,55 @@ class FlashDetector(threading.Thread):
 		self.join()
 		self.gpioTimeReader.cleanup()
 
-class PerTimeSliceFlashCounter():
 
-	def __init__(self, timeSliceDurationInS ):
-		self.timeSliceDurationInS = timeSliceDurationInS
+def getTimeSliceIndex( dateTime, sliceDurationInS ):
+	numSeconds = (dateTime.hour * 3600) + (dateTime.minute * 60) + (dateTime.second) + (dateTime.microsecond/1000000)
+	timeSliceNumber = int(numSeconds / sliceDurationInS)
+	return timeSliceNumber
 
-	def processDateTimes( self, dateTimes ):
-		result = []
-		dateTimeIndex = 0
-		numDateTimes = len(dateTimes)
-		while ( dateTimeIndex<numDateTimes ):
-			numFlashesInSlice = 0
-			sliceStartDate =  dateTimes[dateTimeIndex].date()
-			sliceIndex = self._getTimeSliceIndex( dateTimes[dateTimeIndex] )
-			while ( dateTimeIndex<numDateTimes and self._getTimeSliceIndex(dateTimes[dateTimeIndex])==sliceIndex ):
-				numFlashesInSlice += 1
-				dateTimeIndex += 1
+# Return a tuple hours, minutes, seconds
+def getTimeSliceStartTime( timeSliceIndex, sliceDurationInS ):
+	numSeconds = timeSliceIndex * sliceDurationInS
+	hours = numSeconds / 3600
+	numSeconds -= hours*3600
+	minutes = numSeconds / 60
+	numSeconds -= minutes * 60 
+	seconds = numSeconds
+	return (hours, minutes, seconds)
 
-			sliceStartTime = self._getTimeSliceStartTime(sliceIndex)
-			sliceStartDateTime = datetime.datetime( sliceStartDate.year, sliceStartDate.month, sliceStartDate.day, sliceStartTime[0], sliceStartTime[1], sliceStartTime[2] )
-			#print "#" + str(sliceIndex) + " " + str(sliceStartDateTime) +  " " + str(numFlashesInSlice)
-			result.append( (sliceStartDateTime, numFlashesInSlice) )
-		return result
-
-	def _getTimeSliceIndex( self, dateTime ):
-		numSeconds = (dateTime.hour * 3600) + (dateTime.minute * 60) + (dateTime.second) + (dateTime.microsecond/1000000)
-		timeSliceNumber = int(numSeconds / self.timeSliceDurationInS)
-		return timeSliceNumber
-
-	def _getTimeSliceStartTime( self, timeSliceIndex ):
-		numSeconds = timeSliceIndex * self.timeSliceDurationInS
-		hours = numSeconds / 3600
-		numSeconds -= hours*3600
-		minutes = numSeconds / 60
-		numSeconds -= minutes * 60 
-		seconds = numSeconds
-		return (hours, minutes, seconds)
-
+#class PerTimeSliceFlashCounter():
+#	def __init__(self, timeSliceDurationInS ):
+#		self.timeSliceDurationInS = timeSliceDurationInS
+#
+#	def processDateTimes( self, dateTimes ):
+#		result = []
+#		dateTimeIndex = 0
+#		numDateTimes = len(dateTimes)
+#		while ( dateTimeIndex<numDateTimes ):
+#			numFlashesInSlice = 0
+#			sliceStartDate =  dateTimes[dateTimeIndex].date()
+#			sliceIndex = getTimeSliceIndex( dateTimes[dateTimeIndex], self.timeSliceDurationInS )
+#			while ( dateTimeIndex<numDateTimes and getTimeSliceIndex(dateTimes[dateTimeIndex], self.timeSliceDurationInS)==sliceIndex ):
+#				numFlashesInSlice += 1
+#				dateTimeIndex += 1
+#
+#			sliceStartTime = getTimeSliceStartTime(sliceIndex, self.timeSliceDurationInS )
+#			sliceStartDateTime = datetime.datetime( sliceStartDate.year, sliceStartDate.month, sliceStartDate.day, sliceStartTime[0], sliceStartTime[1], sliceStartTime[2] )
+#			#print "#" + str(sliceIndex) + " " + str(sliceStartDateTime) +  " " + str(numFlashesInSlice)
+#			result.append( (sliceStartDateTime, numFlashesInSlice) )
+#		return result
 		
 class FlashCountSender(): 
 
-	def __init__(self, jsonFileName, worksheetName):
+	def __init__(self, jsonFileName, spreadsheetName, worksheetName):
 		print 'Initializing sender...'
 		json_key = json.load(open(jsonFileName))
 		scope = ['https://spreadsheets.google.com/feeds']
 		credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
 		gc = gspread.authorize(credentials)
-		self.worksheet = gc.open(worksheetName).sheet1
+		self.spreadsheet = gc.open(spreadsheetName)
+		self.worksheet = self.spreadsheet.worksheet(worksheetName)
+		self.worksheet.append_row( ['hihi', 'hoho'] )
 		print 'Sender initialized'
 
 	def sendFlashCounts( self, flashCounts ):
@@ -191,20 +209,56 @@ class Electromon():
 		print 'Electromon!'
 		print '==========='
 		self.gpioTimeReader = GPIOTimeReader(7)
-		self.flashTimes = []
 		self.flashDetector = FlashDetector(self.gpioTimeReader)
-		self.flashCounter = PerTimeSliceFlashCounter(5)
-		self.flashCountSender = FlashCountSender('Electromon-9ec05e526bcd.json', "Electromon");
+		self.sliceDurationInS = 5
+		#self.flashCounter = PerTimeSliceFlashCounter(self.sliceDurationInS)
+		self.flashCountSender = FlashCountSender('Electromon-9ec05e526bcd.json', "Electromon", "A");
 		self.stopRequest = False
 
 	def run(self):
+		lastTimeSliceIndex = getTimeSliceIndex( datetime.datetime.now(), self.sliceDurationInS )
 		self.flashDetector.start()		# Start the detector thread
-		while ( not self.stopRequest ):
-			flashTimes = self.flashDetector.flushFlashTimes()
-			if ( len(flashTimes)>0 ):
-				flashCounts = self.flashCounter.processDateTimes( flashTimes )
-				self.flashCountSender.sendFlashCounts( flashCounts )
-			time.sleep(5)
+		
+		perTimeSliceFlashes = {}
+		while not self.stopRequest:
+
+			time.sleep(3)
+			timeSliceIndex = getTimeSliceIndex( datetime.datetime.now(), self.sliceDurationInS )
+			
+			if timeSliceIndex>lastTimeSliceIndex:
+				
+				# Fetch flash measures from the detector
+				flashTimes = self.flashDetector.flushFlashTimes()
+
+				# Dispatch the flashes per time-slice
+				for flashTime in flashTimes:
+					#print '>>' + str(flashTime)
+					sliceIndex = getTimeSliceIndex( flashTime, self.sliceDurationInS )
+					if perTimeSliceFlashes.has_key(sliceIndex):
+						perTimeSliceFlashes[sliceIndex] += 1
+					else:
+						perTimeSliceFlashes[sliceIndex] = 1
+
+				# Go through each past time-slices (stop before the current one) and 
+				# prepare the final list of flash counts (per slice start datetime)
+				# If a slice didn't have any flash put down a zero for it
+				flashCounts = []
+				sliceStartDate =   datetime.datetime.now().date()		# CRAPPY!!!
+				for sliceIndex in range(lastTimeSliceIndex, timeSliceIndex):
+					
+					sliceStartTime = getTimeSliceStartTime( sliceIndex, self.sliceDurationInS )
+					sliceStartDateTime = datetime.datetime( sliceStartDate.year, sliceStartDate.month, sliceStartDate.day, sliceStartTime[0], sliceStartTime[1], sliceStartTime[2] )
+					if perTimeSliceFlashes.has_key(sliceIndex):
+						flashCounts.append( (sliceStartDateTime, perTimeSliceFlashes[sliceIndex]) ) 
+					else:
+						flashCounts.append( (sliceStartDateTime, 0) ) 
+			
+				# Send flash counts
+				#self.flashCountSender.sendFlashCounts( flashCounts )
+
+				for flashCount in flashCounts:
+					print "Main:>   " + flashCount[0].strftime('%d/%m/%Y %H:%M:%S') + " => " + str(flashCount[1])
+				lastTimeSliceIndex = timeSliceIndex
 
 	def cleanup(self):
 		print 'CLEANUP!'
@@ -226,8 +280,7 @@ def mainLog():
 	finally:
 		flashLogger.cleanup()
 
-
-mainLog()
+main()
 
 def tests():
 	flashTimes = []
